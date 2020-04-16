78037268b5ba109530b24ac5fcb3d4525903a285
Rowan Cockett <rowanc1@gmail.com>
2020-04-16 13:48:41 -0600

    🕶📦Export THROTTLE_SKIP and change theme

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import {
 import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 import { BaseComponent, withInk } from './components/base';
 import * as components from './components';
+import { THROTTLE_SKIP } from './types';
 
 export * from './components';
 export {
   LitElement, html, PropertyDeclaration, PropertyValues,
-  unsafeHTML, BaseComponent, withInk,
+  unsafeHTML, BaseComponent, withInk, THROTTLE_SKIP,
 };
 
 export default components;
